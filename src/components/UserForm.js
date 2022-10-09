765fb3d624b46f8808fc74c8ffcac897fa2506f4
ghager93 <ghager93@gmail.com>
2022-10-10 00:40:32 +1100

    Added material UI formik form.

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,5 +1,6 @@
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Formik, useFormik, Form, Field, ErrorMessage } from "formik";
 import { object, string, ref } from "yup";
+import { TextField, Button } from "@mui/material";
 
 const UserValidation = object().shape({
   username: string().required("Required."),
@@ -8,43 +9,103 @@ const UserValidation = object().shape({
   confirmPassword: string().oneOf([ref('password')], "Passwords do not match.")
 });
 
-const UserForm = () => (
-  <div>
-    <Formik
-      initialValues={{
-        username: "",
-        email: "",
-        password: "",
-        confirmPassword: "",
-      }}
-      validationSchema={UserValidation}
-      onSubmit={(values, { setSubmitting }) => {
-        setTimeout(() => {
-          alert(JSON.stringify(values, null, 2));
-          setSubmitting(false);
-        }, 400);
-      }}
-    > 
-      {({ isSubmitting }) => (
-        <Form>
-          <h1>Sign Up</h1>
-          <Field name="username" />
-          <div>
-            <ErrorMessage name="username" component="div" />
-          </div>
-          <Field type="email" name="email" />
-          <ErrorMessage name="email" component="div" />
-          <Field type="password" name="password" />
-          <ErrorMessage name="password" component="div" />
-          <Field type="password" name="confirmPassword" />
-          <ErrorMessage name="confirmPassword" component="div" />
-          <button type="submit" disabled={ isSubmitting }>
-            Submit
-          </button> 
-        </Form>
-      )}
-    </Formik>
-  </div>
-);
+const UserForm = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: 'foobar@example.com',
+      password: 'foobar',
+    },
+    validationSchema: UserValidation,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
+  return (
+    <div>
+      <Formik
+        initialValues={{
+          username: "",
+          email: "",
+          password: "",
+          confirmPassword: "",
+        }}
+        validationSchema={UserValidation}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}
+      > 
+        {({ isSubmitting }) => (
+          <Form>
+            <h1>Sign Up</h1>
+            <Field name="username" />
+            <div>
+              <ErrorMessage name="username" component="div" />
+            </div>
+            <Field type="email" name="email" />
+            <ErrorMessage name="email" component="div" />
+            <Field type="password" name="password" />
+            <ErrorMessage name="password" component="div" />
+            <Field type="password" name="confirmPassword" />
+            <ErrorMessage name="confirmPassword" component="div" />
+            <button type="submit" disabled={ isSubmitting }>
+              Submit
+            </button> 
+          </Form>
+        )}
+      </Formik>
+      <form onSubmit={formik.handleSubmit}>
+        <TextField
+          fullWidth
+          id="username"
+          name="username"
+          label="Username"
+          value={formik.values.username}
+          onChange={formik.handleChange}
+          error={formik.touched.username && Boolean(formik.errors.username)}
+          helperText={formik.touched.email && formik.errors.username}
+        />
+        <TextField
+          fullWidth
+          id="email"
+          name="email"
+          label="Email"
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          error={formik.touched.email && Boolean(formik.errors.email)}
+          helperText={formik.touched.email && formik.errors.email}
+        />
+        <TextField
+          fullWidth
+          id="password"
+          name="password"
+          label="Password"
+          type="password"
+          value={formik.values.password}
+          onChange={formik.handleChange}
+          error={formik.touched.password && Boolean(formik.errors.password)}
+          helperText={formik.touched.password && formik.errors.password}
+        />
+        <TextField
+          fullWidth
+          id="confirmPassword"
+          name="confirmPassword"
+          label="ConfirmPassword"
+          type="password"
+          value={formik.values.confirmPassword}
+          onChange={formik.handleChange}
+          error={formik.touched.confirmPassword && Boolean(formik.errors.confirmPassword)}
+          helperText={formik.touched.confirmPassword && formik.errors.confirmPassword}
+        />
+        <Button color="primary" variant="contained" fullWidth type="submit">
+          Submit
+        </Button>
+      </form>
+    </div>
+  );
+};
 
 export default UserForm;
